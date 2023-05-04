e6c0f8cdc741699690e3c99823d562420861a1e5
TacticTacos <nikita.baranov.ua@gmail.com>
2023-05-04 23:02:06 +0300

    there is my homework

---
+++ bananas/homework 3 lesson/lesson_3_hw_2.py
@@ -0,0 +1,21 @@
+a = input()
+employee_first_name
+a = "employee"
+b = "first"
+c = "name"
+d = a + "_" + b + "_" + c
+d
+'employee_first_name'
+a = str.capitalize (a)
+b = str.capitalize (b)
+c = str.capitalize (c)
+d = a + "_" + b + "_" + c
+d
+'Employee_First_Name'
+n = d.split("_")
+n
+['Employee', 'First', 'Name']
+print("".join(n))
+EmployeeFirstName
+
+
---
+++ bananas/homework 3 lesson/lesson_3_hw_4.2.py
@@ -0,0 +1,8 @@
+a = input("Enter a number: ")
+Enter a number: 123
+a = int(a)
+b1 = a % 10
+b2 = a % 100 // 10
+b3 = a // 100
+print ("Sum of numbers:", b1 + b2 + b3)
+Sum of numbers: 6
